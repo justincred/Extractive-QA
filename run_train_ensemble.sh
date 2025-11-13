98b57a83f0fbc292504018e0032568bb5ff43a6c
Ibnu Taimiyyah <94895540+ibnu2651@users.noreply.github.com>
2025-11-13 21:08:12 +0800

    Update run_train_ensemble.sh

--- run_train_ensemble.sh
+++ run_train_ensemble.sh
@@ -5,14 +5,8 @@
 #SBATCH -J cs4248
 #SBATCH -o logs/out_%j.out
 nvidia-smi
-#
-# Example. Replace the model_names and weights arg with your own. 
-# srun python preprocess_ensemble.py --model_names microsoft/deberta-v3-base,bert-base-uncased,roberta-base
-# srun python train_ensemble.py --model_names microsoft/deberta-v3-base,bert-base-uncased
-# srun python eval_ensemble.py --model_names microsoft/deberta-v3-base,bert-base-uncased --weights 1.0,1.0
-# srun python evaluate-v2.0.py dev-v1.1.json predictions.json
 
-srun python preprocess_ensemble.py --model_names 
-srun python train_ensemble.py --model_names 
-srun python eval_ensemble.py --model_names --weights 
+srun python preprocess_ensemble.py --model_names <model_names>
+srun python train_ensemble.py --model_names <model_names>
+srun python eval_ensemble.py --model_names <model_names> 
 srun python evaluate-v2.0.py dev-v1.1.json predictions.json
